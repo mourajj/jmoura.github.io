ce7317f0ef8ffb678a1f6d47656baa3104e08493
Jonathan Henrique de Moura <jonathanmourax4@gmail.com>
2022-04-13 09:39:15 -0300

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,14 +7,15 @@ const About = () => {
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
             <p className='text-4xl font-bold inline border-b-4 border-[#ff4415]'>
-              About
+    
             </p>
           </div>
           <div></div>
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Jonathan, nice to meet you.</p>
+              <p>Hi!</p>
+              <p>Nice to meet you.</p>
             </div>
             <div>
               <p>I am currently working as a Product Engineer at SAP, and I'm passionate with programming. 
