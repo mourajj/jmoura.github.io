3eec1dd3359d517bed29632e9502bae979de5343
Jonathan Henrique de Moura <jonathanmourax4@gmail.com>
2023-05-07 19:54:32 -0300

    fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -112,12 +112,6 @@ const Navbar = () => {
                             Github <FaGithub size={30} />
                         </a>
                     </li>
-                    <Link to='contact' smooth={true} duration={500}>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                            <a className='flex justify-between items-center w-full text-gray-300'>
-                                Email <HiOutlineMail size={30} />
-                            </a>
-                    </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#FF3333]'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
@@ -126,7 +120,6 @@ const Navbar = () => {
                             Youtube <FaYoutube size={30} />
                         </a>
                     </li>
-                    </Link>
                 </ul>
             </div>
         </div>
