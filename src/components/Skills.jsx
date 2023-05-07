295bb0d28915a917586e51c5cff96e659cf7caa7
Jonathan Henrique de Moura <jonathanmourax4@gmail.com>
2023-05-07 18:29:23 -0300

    changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -22,7 +22,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-[#ff4415] '>Skills</p>
-              <p className='py-4'>// This is my technology stack</p>
+              <p className='py-4'>// This is my current technology stack</p>
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
